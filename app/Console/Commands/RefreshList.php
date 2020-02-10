22408c351cfbb0a7bb5726bb36cde5c9498bc673
shadowusr <shadow.user@bk.ru>
2020-02-10 12:10:13 -0800

    Implemented basic features of post updating.

--- app/Console/Commands/RefreshList.php
+++ app/Console/Commands/RefreshList.php
@@ -2,7 +2,13 @@
 
 namespace App\Console\Commands;
 
+use App\Models\Group;
+use GuzzleHttp\Client;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Log;
+use Twig\Extension\SandboxExtension;
+use Twig\Loader\ArrayLoader;
+use Twig\Sandbox\SecurityPolicy;
 
 class RefreshList extends Command
 {
@@ -21,10 +27,41 @@ class RefreshList extends Command
     protected $description = 'Refresh lists in all groups.';
 
     /**
+     * Return last post which was made by bot and has to be edited.
+     *
+     * @param Group $group
+     * @return mixed
+     */
+    private function getLastPostIdByGroup(Group $group) {
+        return $group->last_post_id;
+    }
+
+
+    private function generatePostText(Group $group, $users) {
+        // Preparing sandbox for template
+        $whitelist['tags'] = ['if', 'for'];
+        $whitelist['filters'] = [];
+        $whitelist['methods'] = [];
+        $whitelist['properties'] = [];
+        $whitelist['functions'] = [];
+        $policy = new SecurityPolicy($whitelist['tags'], $whitelist['filters'], $whitelist['methods'], $whitelist['properties'], $whitelist['functions']);
+        $sandbox = new SandboxExtension($policy, true);
+
+        // Setting up Twig engine
+        $twigLoader = new ArrayLoader([ 'text' => $group->post_layout ]);
+
+        $twig = new \Twig\Environment($twigLoader);
+        $twig->addExtension($sandbox);
+
+        return $twig->render('text', ['users' => $users]);
+    }
+
+    /**
      * Create a new command instance.
      *
      * @return void
      */
+
     public function __construct()
     {
         parent::__construct();
@@ -45,5 +82,84 @@ class RefreshList extends Command
         4. Generate twig template.
         5. Edit post with new template.
         */
+
+        /*
+         * TODO:
+         * Refactor code: decompose handle method into many slim methods.
+         * Write unit tests.
+         * */
+
+        $api = new Client([
+            'base_uri' => 'https://api.vk.com/method/',
+            'timeout' => 2.0,
+            'form_params' => [
+                'v' => '5.103',
+                'access_token' => env('VK_BOT_ACCESS_TOKEN'),
+            ]
+        ]);
+
+        $groups = Group::all();
+
+        foreach ($groups as $group) {
+            $lastPostId = $this->getLastPostIdByGroup($group);
+            if ($lastPostId) {
+                continue;
+            }
+            $reposts = json_decode($api->post('wall.getReposts', [
+                'form_params' => [
+                    'owner_id' => $group->id,
+                    'post_id' => $lastPostId,
+                    'count' => 100,
+                ],
+            ])->getBody()->getContents(), true);
+
+            if (!$reposts || !isset($reposts['response']['items'])) {
+                Log::error('Unable to get reposts data for group vk.com/club' . -$group->id . '. Dump of vk response: ' . print_r($reposts, true));
+                continue;
+            }
+
+            $viewsByUserId = []; // key = user id, value = views count.
+
+            foreach ($reposts['response']['items'] as $repost) {
+                $viewsByUserId[$repost['from_id']] = $repost['views']['count'];
+            }
+
+            $users = json_decode($api->post('users.get', [
+                'form_params' => [
+                    'user_ids' => implode(',', array_column($reposts['response']['items'], 'from_id')),
+                    'lang' => 'ru',
+                ],
+            ])->getBody()->getContents(), true);
+
+            if (!$users || !isset($users['response'])) {
+                Log::error('Unable to get users data for group vk.com/club' . -$group->id . '. Dump of vk response: ' . print_r($users, true));
+                continue;
+            }
+
+            $users = $users['response'];
+
+            foreach ($users as &$user) {
+                $user['views'] = $viewsByUserId[$user['id']];
+            }
+
+            usort($users, function ($a, $b) {
+                return $b['views'] <=> $a['views'];
+            });
+
+            $newText = $this->generatePostText($group, $users);
+
+            $result = json_decode($api->post('wall.edit', [
+                'form_params' => [
+                    'owner_id' => $group->id,
+                    'post_id' => $lastPostId,
+                    'message' => $newText,
+                ],
+            ])->getBody()->getContents(), true);
+
+            if (!$result || !isset($result['response'])) {
+                Log::error('Editing post in group vk.com/club' . -$group->id . ' failed! Dump of vk response: ' . print_r($result, true));
+                continue;
+            }
+        }
     }
 }
