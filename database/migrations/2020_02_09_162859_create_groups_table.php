7693887a3ff82ae661f00ff0ef50437e20ff4746
shadowusr <shadow.user@bk.ru>
2020-02-10 12:42:54 -0800

    Revert "Updated migration to match command's requirements."
    This reverts commit a0d878b290b10a69ee585ce01285b1bd0f2e4d05.

--- database/migrations/2020_02_09_162859_create_groups_table.php
+++ database/migrations/2020_02_09_162859_create_groups_table.php
@@ -17,7 +17,6 @@ class CreateGroupsTable extends Migration
             $table->integer('id');
             $table->integer('owner_id');
             $table->integer('last_post_id');
-            $table->integer('last_post_time');
             $table->timestamps();
 
             //$table->foreign('owner_id')->references('id')->on('users')->onUpdate('cascade');
